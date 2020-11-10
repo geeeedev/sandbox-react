7d384474a484df4d95d18248a0a371efba6576d4
Gwennie Lau <geeeedev@gmail.com>
2020-11-10 12:04:18 -0800

    +bootstrap

--- src/compOther/Hello.js
+++ src/compOther/Hello.js
@@ -8,11 +8,16 @@ function Hello() {
     <>
       <h1>Hello Dojo!</h1>
       <ul>
-        Things I need to do:
-        <li>Start Freezer App</li>
+        <span style={{fontWeight:"bold",color:"dark grey"}}> Things I need to do:</span>
+        {/* <li>Start Freezer App</li>
         <li>Quick through React chapter</li>
         <li>Do dishes</li>
-        <li>Blow up pool</li>
+        <li>Blow up pool</li> */}
+        <li className='text-success'>Start Google Map API</li>
+        <li style={{color: "red"}}>Start Portfoli with JSON Feed</li>
+        <li className="text-info">Test Json Feed HERE</li>
+        <li className='text-primary'>Clean and Pack Away Pool</li>
+        <li></li>
       </ul>
     </>
   );
