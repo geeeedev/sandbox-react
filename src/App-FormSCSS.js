fb4974acb4ad7c0d69e7f7ae084cfd075bc11dc3
Gwennie Lau <geeeedev@gmail.com>
2021-04-26 13:27:03 -0700

    renamed

---
+++ src/App-FormSCSS.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import "./App.css";
+import LoginFormValidation from "./compFormSCSS/LoginFormValidation";
+
+const App = () => {
+  return (
+    <LoginFormValidation />
+  );
+};
+
+export default App;
